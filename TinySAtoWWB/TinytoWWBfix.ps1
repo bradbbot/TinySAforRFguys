411d95cf352b35fbfa470a62b016d7d2c0b905f1
bradbbot <120657383+bradbbot@users.noreply.github.com>
2022-12-15 10:18:33 -0600

    Update TinytoWWBfix.ps1

--- TinySAtoWWB/TinytoWWBfix.ps1
+++ TinySAtoWWB/TinytoWWBfix.ps1
@@ -1,4 +1,4 @@
-﻿<#load input file#>
+<#load input file#>
 $csv = Import-Csv -path "C:\PATHTOYOUR\INPUTFILELOCATION.csv" -Delimiter ";" -header freq, level 
 
 <#modify data#>
@@ -12,3 +12,5 @@ $Item.level = [math]::Round($Item.level,0)
 $Csv | Export-Csv -Path "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv" -Delimiter ‘,’ -NoType 
 <#remove quotation marks#>
 (Get-Content "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv") -replace '"' | Set-Content "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv"
+Get-ChildItem "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv" | ForEach-Object {          
+Rename-Item $_.FullName "$BackupFolder$($_.BaseName -replace " ", "_" -replace '\..*?$')-$(Get-Date -Format "HHmmMMddyyyy").csv"
