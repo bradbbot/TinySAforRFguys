31700e1a970fe0c284c97f9e24923432b55dda44
bradbbot <120657383+bradbbot@users.noreply.github.com>
2022-12-15 15:02:13 -0600

    Adds IAS compatibility
    This version replaces TinysatoWWB.  CSV's from this convertor are compatible with both IAS and Shure WWB.

---
+++ TinySAtoIAS/TinytoIASfix.ps1
@@ -0,0 +1,16 @@
+﻿<#load input file#>
+$csv = Import-Csv -path "C:\PATHTOYOUR\INPUTFILENAME.csv" -Delimiter ";" -header freq, level 
+
+<#modify data#>
+foreach ($Item in $csv)
+{
+$Item.freq = [math]::Round($Item.freq/1000000,3) 
+$Item.level = [math]::Round($Item.level,0)
+}
+
+<#export file#>
+$Csv | Export-Csv -Path "C:\PATHTOYOUR\OUTPUTFILENAME.csv" -Delimiter ‘,’ -NoType 
+<#remove quotation marks#>
+(Get-Content "C:\PATHTOYOUR\OUTPUTFILENAME.csv") -replace '"' | select -Skip 1 | Set-Content "C:\PATHTOYOUR\OUTPUTFILENAME.csv"
+Get-ChildItem "C:\PATHTOYOUR\OUTPUTFILENAME.csv" | ForEach-Object {          
+Rename-Item $_.FullName "$BackupFolder$($_.BaseName -replace " ", "_" -replace '\..*?$')-$(Get-Date -Format "HHmmMMddyyyy").csv"}
---
+++ TinySAtoIAS/tinytoias.bat
@@ -0,0 +1,1 @@
+powershell C:\PATHTOYOUR\TinytoIASfix.ps1
