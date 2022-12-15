d5f29574948b65b866c798ddd7bc89c09337e6ab
bradbbot <120657383+bradbbot@users.noreply.github.com>
2022-12-15 10:15:50 -0600

    Time and Date
    This revision removed the need for Powershell 7 and adds a time and date stamp to the created file.

---
+++ TinySAtoWWB/TinySAtoWWB/TinytoWWBfix.ps1
@@ -0,0 +1,16 @@
+﻿<#load input file#>
+$csv = Import-Csv -path "C:\PATHTOYOUR\INPUTFILELOCATION.csv" -Delimiter ";" -header freq, level 
+
+<#modify data#>
+foreach ($Item in $csv)
+{
+$Item.freq = [math]::Round($Item.freq/1000000,3) 
+$Item.level = [math]::Round($Item.level,0)
+}
+
+<#export file#>
+$Csv | Export-Csv -Path "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv" -Delimiter ‘,’ -NoType 
+<#remove quotation marks#>
+(Get-Content "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv") -replace '"' | Set-Content "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv"
+Get-ChildItem "C:\PATHTOYOUR\OUTPUTFILELOCATION.csv" | ForEach-Object {          
+Rename-Item $_.FullName "$BackupFolder$($_.BaseName -replace " ", "_" -replace '\..*?$')-$(Get-Date -Format "HHmmMMddyyyy").csv"
---
+++ TinySAtoWWB/TinySAtoWWB/tinytowwb.bat
@@ -0,0 +1,1 @@
+powershell C:\PATHTOYOUR\TinytoWWBfix.ps1
